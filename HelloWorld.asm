7acab6cccfdd4e9d7c8597924f936d0c269728cd
dvir019 <30556126+dvir019@users.noreply.github.com>
2019-10-19 21:07:20 +0300

    Create Hello World file in assembly 8086

---
+++ HelloWorld.asm
@@ -0,0 +1,25 @@
+data segment
+    pkey db "Hello World$"
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+
+            
+    lea dx, pkey
+    mov ah, 9
+    int 21h
+    
+    
+    mov ax, 4c00h
+    int 21h    
+ends
+
+end start
